32aea5e09f5bde5a6e09bb16bcd43e135450a629
Hamza <hamzakamboh82@gmail.com>
2025-09-06 16:22:29 +0500

    Commit after adding functionality

---
+++ packaged-app/src/packaged_app/myfile.py
@@ -0,0 +1,9 @@
+def sample():
+    name = "Muhammad Hamza Ittifaq"
+    roll_no = "PIAIC259224"
+    print(name)
+    print(roll_no)
+    print(f"Name (Upper Case): {name.upper()}")
+    print(f"Name (Lower Case): {name.lower()}")
+
+    
