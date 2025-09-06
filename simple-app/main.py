77f85665a025cc3f8d1a373aab3ffd954b43306f
Hamza <hamzakamboh82@gmail.com>
2025-09-06 16:20:55 +0500

    Commit after adding functionality

--- simple-app/main.py
+++ simple-app/main.py
@@ -1,6 +1,10 @@
 def main():
-    print("Hello from simple-app!")
-
+    name = "Muhammad Hamza Ittifaq"
+    roll_no = "PIAIC259224"
+    print(name)
+    print(roll_no)
+    print(f"Name (Upper Case): {name.upper()}")
+    print(f"Name (Lower Case): {name.lower()}")
 
 if __name__ == "__main__":
     main()
